0d3dd2469598087589b19caecfdae5a789372792
Fran Melendo <jamelrom@gmail.com>
2015-10-26 22:29:54 +0100

    Paso a colecciones

--- client/plantillas/partes/partes_lista.js
+++ client/plantillas/partes/partes_lista.js
@@ -1,17 +1,5 @@
-var partesDatos = [
-  {
-    alumno: 'Antonio',
-    profesor: 'Melendo'
-  },
-  {
-    alumno: 'Jesus',
-    profesor: 'Marisa'
-  },
-  {
-    alumno: 'Manolo',
-    profesor: 'Rosa'
-  }
-];
 Template.partesLista.helpers({
-  partes: partesDatos
+  partes: function(){
+     return Partes.find();
+   }
 });
--- server/datos_relleno.js
+++ server/datos_relleno.js
@@ -1,8 +1,42 @@
 if (Meteor.users.find().count() === 0) {
-
-
-  var tomId = Meteor.users.insert({
+  var jefeid = Meteor.users.insert({
+    username: 'jefe',
     profile: { nombre: 'Jefe', perfil:'Jefe Estudios' }
   });
+  Accounts.setPassword(jefeid, 'jefe');
+}
 
+if(Partes.find().count() === 0) {
+    Partes.insert({
+      curso_id: '1',
+      curso: '1º ESO A',
+      alumno_id: '1',
+      alumno: 'Francisco Gómez Gonzalez',
+      profesor_id: '1',
+      profesor: 'Francisco Javier Melendo Roman',
+      gravedad: 'leve',
+      comentario: 'El niño ha tirado un papel al suelo'
+    });
+
+    Partes.insert({
+      curso_id: '1',
+      curso: '1º ESO B',
+      alumno_id: '1',
+      alumno: 'Antonia Garcia Gonzalez',
+      profesor_id: '1',
+      profesor: 'Rosa Calvo',
+      gravedad: 'grave',
+      comentario: 'El niño ha tirado un papel a la cara del compañero'
+    });
+
+    Partes.insert({
+      curso_id: '1',
+      curso: '2º ESO A',
+      alumno_id: '1',
+      alumno: 'Ermenegildo Parras',
+      profesor_id: '1',
+      profesor: 'Marisa Perula',
+      gravedad: 'muy grave',
+      comentario: 'El niño ha pegado a lotro compañero'
+    });
 }
